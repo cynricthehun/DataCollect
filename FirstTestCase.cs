70b503ae2256b2128edc7d87b2b6d9b876c81b82
Joey Meyer <joeymeyer1986@gmail.com>
2018-11-10 15:50:41 -0600

    updated code

--- FirstTestCase.cs
+++ FirstTestCase.cs
@@ -9,56 +9,74 @@ namespace IFCDATA
     {
         static void Main(string[] args)
         {
+            int[] AddressCollections = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             Console.WriteLine("Hello World!");
             var NotRegisteredAddress = "";
             var RegisteredAddress = "";
             IWebDriver driver = new ChromeDriver(@"C:\Program Files (x86)\Google\Chrome\Application");
             // Open the website.
             driver.Url = "https://awesomefiber.com/awesome-fiber-address-search/";
-
-            IWebElement houseNumberElement = driver.FindElement(By.Id("chkpt-lookup-house-number"));
-            houseNumberElement.SendKeys("1");
-            IWebElement zipCodeElement = driver.FindElement(By.Id("chkpt-lookup-zip-code"));
-            zipCodeElement.SendKeys("1");
-            IWebElement lookUpButton = driver.FindElement(By.ClassName("uk-button"));
-            lookUpButton.Click();
-
-            System.Threading.Thread.Sleep(5000);
-
-            IWebElement addressSelectList = driver.FindElement(By.ClassName("uk-select"));
-
-            if (addressSelectList != null)
+            for( int j = 1; j <= AddressCollections.Length; j++)
             {
-                SelectElement oSelect = new SelectElement(driver.FindElement(By.ClassName("uk-select")));
-                
-                for (int i = 0; i < oSelect.Options.Count; i++)
+                IWebElement houseNumberElement = driver.FindElement(By.Id("chkpt-lookup-house-number"));
+                houseNumberElement.Clear();
+                houseNumberElement.SendKeys(j.ToString());
+                IWebElement zipCodeElement = driver.FindElement(By.Id("chkpt-lookup-zip-code"));
+                zipCodeElement.Clear();
+                zipCodeElement.SendKeys("0");
+                IWebElement lookUpButton = driver.FindElement(By.ClassName("uk-button"));
+                lookUpButton.Click();
+
+                System.Threading.Thread.Sleep(5000);
+                bool FoundAddressSelectList = false;
+                // IWebElement addressSelectList = driver.FindElement(By.ClassName("uk-select"));
+                try
                 {
-                    oSelect.SelectByIndex(i);
-                    String sValue = oSelect.SelectedOption.Text;
-                    Console.Write(sValue);
-                    //document.getElementById("chkpt-address-confirm") != null ? true : false;
-                    var NextButtonExist = driver.FindElement(By.Id("chkpt-address-confirm"));
-                    if (NextButtonExist != null)
-                    {
-                        NotRegisteredAddress += sValue + ", ";
-                    } else
-                    {
-                        RegisteredAddress += sValue + ", ";
-                    }
-                    // Console.Write(driver.FindElement(By.ClassName("uk-select")).Text);
-                    //Console.Write();
+                    driver.FindElement(By.ClassName("uk-select"));
+                    FoundAddressSelectList = true;
                 }
-                Console.Write("Not Registered Addresses: ");
-                Console.Write(NotRegisteredAddress);
-                Console.Write("/n");
-                Console.Write("Registered Addresses");
-                Console.Write(RegisteredAddress);
-                Console.Write("/n");
+                catch (NoSuchElementException)
+                {
+                    FoundAddressSelectList = false;
+                }
+
+                if (FoundAddressSelectList != false)
+                {
+                    bool foundRecord = false;
+                    SelectElement oSelect = new SelectElement(driver.FindElement(By.ClassName("uk-select")));
+
+                    for (int i = 0; i < oSelect.Options.Count; i++)
+                    {
+                        oSelect.SelectByIndex(i);
+                        String sValue = oSelect.SelectedOption.Text;
+                        Console.Write(sValue);
+                        try
+                        {
+                            driver.FindElement(By.Id("chkpt-address-confirm"));
+                            foundRecord = true;
+                        }
+                        catch (NoSuchElementException)
+                        {
+                            foundRecord = false;
+                        }
+
+                        if (foundRecord == true)
+                        {
+                            NotRegisteredAddress += sValue + ", ";
+                        }
+                        else
+                        {
+                            RegisteredAddress += sValue + ", ";
+                        }
+                    }
+                    Console.Write("Not Registered Addresses: ");
+                    Console.Write(NotRegisteredAddress);
+                    Console.Write("\n");
+                    Console.Write("Registered Addresses");
+                    Console.Write(RegisteredAddress);
+                    Console.Write("\n");
+                }
             }
-
-            
-
-
             // Close the website
             // driver.Close();
         }
